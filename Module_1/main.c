a1e290891eae0a53052f02a37e3064938a22a26e
SREERAJ KRISHNA <107305138+SREERAJSVT@users.noreply.github.com>
2025-08-05 01:04:36 +0530

    new functions
    added print

--- Module_1/main.c
+++ Module_1/main.c
@@ -4,13 +4,13 @@ int main()
 {
    // std::cout << "Hello World" << std::endl;
 char name[50];
-string address[100];
+char address[100];
    printf("Enter your name:\n");
    scanf("%s", name);
    printf("Enter your address:\n");
    scanf("%s", address);
    printf("You entered: %s\n", name);
-   printf("You entered: %s\n", address);
+   printf_s("You entered: %s\n", address);
    return 0;
 }
 // This is a simple C++ program that prints&read "Address" to the console.
