e66cc9709ba0d70b1749b12c7673c468ed7ceda9
SREERAJ KRISHNA <107305138+SREERAJSVT@users.noreply.github.com>
2025-08-05 00:51:01 +0530

    adding change to cpp to c
    changeing cpp to c

---
+++ Module_1/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h> 
+int main()
+{
+   // std::cout << "Hello World" << std::endl;
+char name[50];
+string address[100];
+   printf("Enter your name:\n");
+   scanf("%s", name);
+   printf("Enter your address:\n");
+   scanf("%s", address);
+   printf("You entered: %s\n", name);
+   printf("You entered: %s\n", address);
+   return 0;
+}
+// This is a simple C++ program that prints&read "Address" to the console.
--- Module_1/main.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-
-int main()
-{
-    std::cout << "Hello World" << std::endl;
-}
-// This is a simple C++ program that prints "Hello World" to the console.
