a2b8e3b99b812550aba96666c7108363184c6ceb
SREERAJ KRISHNA <107305138+SREERAJSVT@users.noreply.github.com>
2025-09-24 21:43:39 +0530

    avr  pwm
    		OCR0A =128;//Duty Cycle

--- AVR/Week9/PWM Live/PWM Live/main.c
+++ AVR/Week9/PWM Live/PWM Live/main.c
@@ -6,9 +6,7 @@ int main(void) {
 		TCCR0A |= (1 << COM0A1);
 		OCR0A =128;//Duty Cycle
 		TCCR0B |= (1 << CS01) | (1 << CS00);
-
 while (1) {
-	// The microcontroller is free to execute other tasks.
 }
 }
 
