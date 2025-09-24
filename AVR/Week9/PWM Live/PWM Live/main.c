6167d1e4116a090198f68469ef0c9ebf0122c51b
SREERAJ KRISHNA <107305138+SREERAJSVT@users.noreply.github.com>
2025-09-24 21:38:58 +0530

    avr pwm
    AVR PWM

---
+++ AVR/Week9/PWM Live/PWM Live/main.c
@@ -0,0 +1,15 @@
+#include <avr/io.h>
+
+int main(void) {
+	    DDRD |= (1 << DDD6);
+		TCCR0A |= (1 << WGM01) | (1 << WGM00);
+		TCCR0A |= (1 << COM0A1);
+		OCR0A =128;//Duty Cycle
+		TCCR0B |= (1 << CS01) | (1 << CS00);
+
+while (1) {
+	// The microcontroller is free to execute other tasks.
+}
+}
+
+//home work:write a program fade; loop used.
